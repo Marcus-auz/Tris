87492eddb38d0415ffceb931254f97e7d043a7ea
Marcus-auz <oo7marcusaurelius@gmail.com>
2019-11-14 02:11:41 +0530

    sending message and attachments

--- bot.js
+++ bot.js
@@ -10,7 +10,18 @@ client.on('ready',()=>{
     //Gives the server name
     client.guilds.forEach((guild)=>{
         console.log(guild.name)
+        guild.channels.forEach((channel)=>{
+            console.log(`-${channel.name} ${channel.type} ${channel.id}`)
+        })
+        //general channel id 644269920070271018
     })
+
+    //sending message to server
+    let generalChannel =client.channels.get("644269920070271018")
+    const attachment=new Discord.Attachment("https://images.search.yahoo.com/yhs/search?p=devwloper+image&fr=yhs-ddc-linuxmint&hspart=ddc&hsimp=yhs-linuxmint&imgurl=https%3A%2F%2Fmadebysidecar.com%2Fassets%2Fproducts%2Fthumbs%2Fdeveloper-photo-bundle-4.jpg#id=14&iurl=https%3A%2F%2Fmadebysidecar.com%2Fassets%2Fproducts%2Fthumbs%2Fdeveloper-photo-bundle-4.jpg&action=click")
+    generalChannel.send(attachment) //if attachment passed it send attach ,u can send message too
+
+    
 })
 
 client.login("NjQ0MjcxNTczNjUzNTIwMzk3.XcxnSQ.8SyQ0N_FbApweS2M74JZnGG11ao");
