907e2cd7fcfdc96bf8bac61b7584290b8d12e577
Marcus-auz <oo7marcusaurelius@gmail.com>
2019-11-14 03:57:39 +0530

    help command basic functionality is added

--- bot.js
+++ bot.js
@@ -20,30 +20,54 @@ client.on('ready',()=>{
     let generalChannel =client.channels.get("644269920070271018")
     const attachment=new Discord.Attachment("https://images.search.yahoo.com/yhs/search?p=devwloper+image&fr=yhs-ddc-linuxmint&hspart=ddc&hsimp=yhs-linuxmint&imgurl=https%3A%2F%2Fmadebysidecar.com%2Fassets%2Fproducts%2Fthumbs%2Fdeveloper-photo-bundle-4.jpg#id=14&iurl=https%3A%2F%2Fmadebysidecar.com%2Fassets%2Fproducts%2Fthumbs%2Fdeveloper-photo-bundle-4.jpg&action=click")
     generalChannel.send(attachment) //if attachment passed it send attach ,u can send message too
-
+})
 
     client.on('message',(receivedMessage)=>{
-        if(receivedMessage.author==client.user) return //this condition is set so that bot dont keep replying its own messages and enter a infinte loop 
-        else {
+        if(receivedMessage.author==client.user){
+            return//this condition is set so that bot dont keep replying its own messages and enter a infinte loop 
+        }
             //it tags the user who sent the msg and echo the msg back
-            receivedMessage.channel.send("Message received," + receivedMessage.author.toString() + ":  " + receivedMessage.content)
+            //receivedMessage.channel.send("Message received," + receivedMessage.author.toString() + ":  " + receivedMessage.content)
             
             //adding a reaction to a message
-            receivedMessage.react("😁") //emoji reaction to the msg
+            //receivedMessage.react("😁") //emoji reaction to the msg
 
             //reacting with a custom emoji {you need to first add custom emoji into the server}
             //react with each custom emoji
-            receivedMessage.guild.emojis.forEach(customEmoji=>{
-                console.log(`${customEmoji.name} ${customEmoji.id}`)
-                receivedMessage.react(customEmoji)
-            })
+            //receivedMessage.guild.emojis.forEach(customEmoji=>{
+            //    console.log(`${customEmoji.name} ${customEmoji.id}`)
+            //    receivedMessage.react(customEmoji)
+            //  })
 
             //to react with some specefic emoji
-            let customEmoji=receivedMessage.guild.emojis.get("emoji id")
-            receivedMessage.react(customEmoji)
-        
-        }
-    })
-})
+            //let customEmoji=receivedMessage.guild.emojis.get("emoji id")
+            //receivedMessage.react(customEmoji)
+    
+        if(receivedMessage.content.startsWith("!")){
+            processCommand(receivedMessage)
+        }  
+    })   
+   
+
+    
+
+function processCommand(receivedMessage){
+    let fullCommand=receivedMessage.content.substr(1)
+    let splitCommand=fullCommand.split(" ") //return array with each word
+    let primaryCommand=splitCommand[0]
+    let arguments=splitCommand.slice(1) //exclude first element and keeps the rest
+
+    if(primaryCommand=="help"){
+        helpCommand(arguments,receivedMessage)
+    }
+}
+
+function helpCommand(arguments,receivedMessage){
+    if(arguments.length==0){
+        receivedMessage.channel.send("Bad request,try `!help [topic]` ")
+    }else{
+        receivedMessage.channel.send("help is here "+arguments)
+    }
+}
 
 client.login("NjQ0MjcxNTczNjUzNTIwMzk3.XcxnSQ.8SyQ0N_FbApweS2M74JZnGG11ao");
